6cebb73b543ad3654730fbc7f189f8c214659ef2
Fahad Hossain <set.name.fahad@gmail.com>
2022-10-17 22:02:14 +0200

    feat: add firefox support
    trim out everything until it works on firefox

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
-import './index.css';
-
-const content = document.body.textContent;
+import App from './App'
+import './index.css'
 
 try {
-    const jsonData = JSON.parse(content.trim());
-    window.json = jsonData;
-    const root = document.createElement('div');
-    root.setAttribute('id', 'rbrahul-awesome-json');
-    document.body.innerHTML = '';
-    document.body.appendChild(root);
-    ReactDOM.render(
-        <App json={jsonData} />,
-        document.getElementById('rbrahul-awesome-json'),
-    );
+  const content = document.body.textContent
+  const jsonData = JSON.parse(content.trim())
+  window.json = jsonData
+  const root = document.createElement('div')
+  root.setAttribute('id', 'rbrahul-awesome-json')
+  document.body.innerHTML = ''
+  document.body.appendChild(root)
+  ReactDOM.render(
+    <App json={jsonData} />,
+    document.getElementById('rbrahul-awesome-json')
+  )
 } catch (e) {
-    // console.error('JSON parsing failed');
+  console.error('JSON parsing failed', e);
 }
